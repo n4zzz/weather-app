6ee3b6c8c14397495a1c2b260c5a54c139ed8fff
n4zzz <nazrinadanan02@gmail.com>
2025-12-29 16:52:39 +0800

    Add fallback mechanism if api key error and search is empty

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -33,7 +33,11 @@ const Weather = () => {
         "13n": snow_icon,
     }
 
-    const search = async (city)=> {
+    const search = async (city) => {
+        if (city === "") {
+            alert("Enter a city")
+            return;
+        }
         try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_OPEN_WEATHER_API}`;
 
@@ -41,7 +45,7 @@ const Weather = () => {
             const data = await response.json();
             console.log(data);
             const icon = allIcons[data.weather[0].icon] || clear_icon;
-            
+
             setWeatherData({
                 humidity: data.main.humidity,
                 windSpeed: data.wind.speed,
@@ -52,42 +56,47 @@ const Weather = () => {
 
 
         } catch (error) {
-            
+
         }
     }
 
     useEffect(() => {
         search("London");
-    },[])
+    }, [])
 
-  return (
-    <div className='weather'>
-        <div className="search-bar">
-            <input ref={inputRef} type="text" placeholder='Search'/>
-            <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
-        </div>
-        <img src={weatherData.icon} alt="" className='weather-icon'/>
-        <p className='temperature'>{weatherData.temperature}°C</p>
-        <p className='location'>{weatherData.location}</p>
-        
-        <div className="weather-data">
-            <div className="col">
-                <img src={humidity_icon} alt="" />
-                <div>
-                    <p>{weatherData.humidity} %</p>
-                    <span>Humidity</span>
-                </div>
+    return (
+        <div className='weather'>
+            <div className="search-bar">
+                <input ref={inputRef} type="text" placeholder='Search' />
+                <img src={search_icon} alt="" onClick={() => search(inputRef.current.value)} />
             </div>
-            <div className="col">
-                <img src={wind_icon} alt="" />
-                <div>
-                    <p>{weatherData.windSpeed} Km/h</p>
-                    <span>Wind Speed</span>
+
+            {weatherData ? <>
+                <img src={weatherData.icon} alt="" className='weather-icon' />
+                <p className='temperature'>{weatherData.temperature}°C</p>
+                <p className='location'>{weatherData.location}</p>
+
+                <div className="weather-data">
+                    <div className="col">
+                        <img src={humidity_icon} alt="" />
+                        <div>
+                            <p>{weatherData.humidity} %</p>
+                            <span>Humidity</span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <img src={wind_icon} alt="" />
+                        <div>
+                            <p>{weatherData.windSpeed} Km/h</p>
+                            <span>Wind Speed</span>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </> : <></>}
+
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default Weather
